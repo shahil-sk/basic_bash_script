28e2948a6c11af7b6eeb022a61f1639160c3c7df
SHL-AHD <93572629+shahil-sk@users.noreply.github.com>
2024-02-17 11:22:52 +0530

    palindrome.sh

--- palindrome.sh
+++ palindrome.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-read -p “Enter a string: ” input_string
-
+echo -e “Enter a string: \c”
+read input_string
 # Reverse the string and compare with the original
 
-if [[ “$input_string” == “$(rev <<< “$input_string”)” ]]; then
+if [[ $input_string == $(rev <<< $input_string) ]]; then
 echo “The entered string ‘$input_string’ is a palindrome.”
 else
-echo “The entered string ‘$input_string’ is not a palindrome.”
+echo “The entered string ‘$input_string’ is not a palindro>
 fi
