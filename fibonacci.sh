ea33e7d80e5d4126416b416a3a472f050e0cff76
SHL-AHD <93572629+shahil-sk@users.noreply.github.com>
2024-02-27 21:25:43 +0530

    lab  list

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-# Script to generate Fibonacci series up to N
-
-# Ask user for N
+#input from the user
 echo "Enter the number of terms in the Fibonacci series:"
 read N
 
-# Initialize the first two Fibonacci numbers
+# initialize first 2 fibonacci variables
 a=0
 b=1
-
-echo "The Fibonacci series is:"
+echo "The Fibonacci series is : "
 
 # Loop to generate the Fibonacci series
 for (( i=0; i<N; i++ ))
@@ -20,5 +16,4 @@ do
     a=$b
     b=$fn
 done
-
-echo "" # Newline for clean output
+echo ""
