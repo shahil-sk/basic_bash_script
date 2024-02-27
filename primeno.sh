c2bb41bc0d53bc7a57604f2db3ebd2956be5e2f4
SHL-AHD <93572629+shahil-sk@users.noreply.github.com>
2024-02-27 21:21:31 +0530

    lab list

--- primeno.sh
+++ primeno.sh
@@ -1,32 +1,26 @@
-#storing the number to be checked 
-number=43 
+#input from the user
+echo -e "Enter a Number : \c"
+read number=
+
 i=2 
- 
 #flag variable 
 f=0 
  
 #running a loop from 2 to number/2 
-
 while test $i -le `expr $number / 2` 
 do
- 
 #checking if i is factor of number 
-
 if test `expr $number % $i` -eq 0 
 then
 f=1 
 fi
- 
 #increment the loop variable 
-
 i=`expr $i + 1` 
 done
 
 if test $f -eq 1 
 then
-
 echo "Not Prime"
 else
-
 echo "Prime"
 fi
